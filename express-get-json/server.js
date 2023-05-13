d6c4d669f732fca2f78cf84aa369c10077699d71
codewithbingle <codewithbingle@gmail.com>
2023-05-13 16:52:38 +0000

    Completed express-get-json exercise.

---
+++ express-get-json/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+
+var grades =
+{
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+var app = express();
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
+
+app.get('/api/grades', (req, res) => {
+  const array = [];
+  for (const key in grades) {
+    array.push(grades[key]);
+  }
+  res.json(array);
+});
