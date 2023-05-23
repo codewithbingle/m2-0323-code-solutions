6910773b488fcbf9ccee74720c986dc242cac778
codewithbingle <codewithbingle@gmail.com>
2023-05-23 04:11:51 +0000

    Updated for two-kilmers.sql

--- sql-select/two-kilmers.sql
+++ sql-select/two-kilmers.sql
@@ -1,6 +1,7 @@
 -- Get the actorId and firstName of only two actors with the lastName of 'Kilmer'.
 
 select  "actorId",
-        "firstName",
-  from "actors"
-  where "lastName" = 'Kilmer';
+        "firstName"
+  from  "actors"
+  where "lastName" = 'Kilmer'
+  limit 2;
